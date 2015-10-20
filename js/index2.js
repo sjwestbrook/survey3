ec2831728e5c07b0eff54d39ef41665898a8253c
sjwestbrook <sawest4291@gmail.com>
2015-10-20 09:04:36 -0600

    css

--- js/index2.js
+++ js/index2.js
@@ -1,6 +1,8 @@
 require('./app2.js');  
 require('./controllers2.js'); 
 require('./services.js');
+require('./css/style4.less');
+
 
 //running webpack --> go into directory where index.js is stored and run webpack
 
